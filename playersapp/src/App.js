22402c66c79a326451d602b2febf622717c9d5c0
carlos <cgarciapieto@gmail.com>
2019-04-02 15:51:19 -0500

    updated

---
+++ playersapp/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Players from "./Players"
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+
+
+          <Players whichPlayer="1"/>
+          <Players whichPlayer="2"/>
+        </div>
+    );
+  }
+}
+
+export default App;
